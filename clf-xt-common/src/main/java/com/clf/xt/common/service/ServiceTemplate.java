744e496465fc34dc14b7a30a571e6300aa4436a7
cookielife <1746497252@qq.com>
2022-02-04 01:03:00 +0800

    Initial commit

---
+++ clf-xt-common/src/main/java/com/clf/xt/common/service/ServiceTemplate.java
@@ -0,0 +1,23 @@
+package com.clf.xt.common.service;
+
+import com.clf.xt.common.model.CallResult;
+
+public interface ServiceTemplate {
+
+
+    /**
+     * run in  datasource and execute Transaction
+     * @param action
+     * @param <T>
+     * @return
+     */
+    <T> CallResult<T> execute(TemplateAction<T> action);
+
+    /**
+     * run in  datasource and not execute Transaction
+     * @param action
+     * @param <T>
+     * @return
+     */
+    <T> CallResult<T> executeQuery(TemplateAction<T> action);
+}
